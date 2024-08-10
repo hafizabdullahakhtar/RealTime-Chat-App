a92cb4b1d4edf8258d833ada11c2b008642e83bc
hafizabdullahakhtar <mailsendkrdo@gmail.com>
2024-08-10 12:04:33 +0500

    Update notification_access_token.dart

--- lib/api/notification_access_token.dart
+++ lib/api/notification_access_token.dart
@@ -20,22 +20,7 @@ class NotificationAccessToken {
         // > Click on 'Generate new private key' Btn & Json file will be downloaded
 
         // Paste Your Generated Json File Content
-        ServiceAccountCredentials.fromJson({
-          "type": "service_account",
-          "project_id": "Your_Project_ID",
-          "private_key_id": "Project_Key_ID",
-          "private_key": "-----yOUR PRIVATE KEY-----",
-          "client_email":
-              "firebase-adminsdk-9y8mc@we-chat-75f13.iam.gserviceaccount.com",
-          "client_id": "115556782220674603906",
-          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-          "token_uri": "https://oauth2.googleapis.com/token",
-          "auth_provider_x509_cert_url":
-              "https://www.googleapis.com/oauth2/v1/certs",
-          "client_x509_cert_url":
-              "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-9y8mc%40we-chat-75f13.iam.gserviceaccount.com",
-          "universe_domain": "googleapis.com"
-        }),
+        ServiceAccountCredentials.fromJson({}),
         [fMessagingScope],
       );
 
